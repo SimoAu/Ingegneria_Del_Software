ff1a526cc30f80fc6580204373729779c3731ab2
SimoAufi <simone.aufiero1@studenti.unimi.it>
2021-11-03 10:52:22 +0100

    +aggiunta classe data utile per costruire le successive classi

---
+++ progettoIngegneriaArtifactID/src/main/java/progettoIngegneriaArtifactID/data.java
@@ -0,0 +1,30 @@
+package progettoIngegneriaArtifactID;
+
+public class data {
+	
+	int giorno;
+	int mese;
+	int anno;
+
+	public data(int giorno, int mese, int anno) {
+	
+		this.giorno = giorno;
+		this.mese = mese;
+		this.anno = anno;
+		
+	}
+
+	public int getGiorno() {
+		return giorno;
+	}
+
+	public int getMese() {
+		return mese;
+	}
+
+
+	public int getAnno() {
+		return anno;
+	}
+
+}
