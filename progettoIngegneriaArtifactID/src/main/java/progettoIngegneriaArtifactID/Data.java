d8296f32cba50057415c979d91fc8164e3aad086
AlessandroCiummei <alessandro.ciummei@studenti.unimi.it>
2021-11-17 11:03:23 +0100

    modifiche alla classe elettore
    +aggiunti attributi alla classe Elettore
    +aggiunto metodo di controllo per la maggiore età nella classe Data
    +aggiunto metodo per esprimere il voto nella classe elettore

--- progettoIngegneriaArtifactID/src/main/java/progettoIngegneriaArtifactID/Data.java
+++ progettoIngegneriaArtifactID/src/main/java/progettoIngegneriaArtifactID/Data.java
@@ -1,5 +1,7 @@
 package progettoIngegneriaArtifactID;
 
+import java.util.Calendar;
+
 public class Data {
 	
 	int giorno;
@@ -28,9 +30,27 @@ public class Data {
 		this.anno=anno;
 	}
 
-
 	public int getAnno() {
 		return anno;
+	}
+
+	public boolean isAdult(){
+
+		Calendar calendario= Calendar.getInstance();
+		if((calendario.YEAR-anno)>18){ return true;}
+
+		if((calendario.YEAR-anno)==18){
+
+			if((calendario.MONTH-mese)>0){ return true;}
+
+			if((calendario.MONTH-mese)==0){
+
+				if((calendario.DAY_OF_MONTH-giorno)>=0){ return true; }
+			}
+
+		}
+
+		return false;
 	}
 
 }
--- progettoIngegneriaArtifactID/src/main/java/progettoIngegneriaArtifactID/Elettore.java
+++ progettoIngegneriaArtifactID/src/main/java/progettoIngegneriaArtifactID/Elettore.java
@@ -5,16 +5,25 @@ public class Elettore{
     String nome;
     String cognome;
     Data nascita= new Data();
+    String nazione;
+    String comune;
+    char sesso;
+    char [] codiceF= new char[16];
+    boolean voto;
     boolean diritto_voto;
 
-    public Elettore (String nome, String cognome, int giorno, int mese, int anno, boolean diritto_voto){
+    public Elettore (String nome, String cognome, int giorno, int mese, int anno, String nazione, String comune, char sesso, char[] codiceF){
 
         this.nome= nome;
         this.cognome=cognome;
         this.nascita.setGiorno(giorno);
         this.nascita.setMese(mese);
         this.nascita.setAnno(anno);
-        this.diritto_voto=diritto_voto;
+        this.nazione= nazione;
+        this.comune= comune;
+        this.sesso= sesso;
+        for (int i=0; i<16 ; i++) { this.codiceF[i] = codiceF[i]; }
+        this.diritto_voto= this.nascita.isAdult();
     }
 
     public String getNome(){
@@ -33,5 +42,20 @@ public class Elettore{
         return this.diritto_voto; 
     }
 
+    public String getNazione() {return this.nazione;}
+
+    public String getComune() { return this.comune; }
+
+    public char getSesso() { return this.sesso; }
+
+    public char[] getCodiceF() { return codiceF; }
+
+    public void esprimi_Voto() {
+
+        if(diritto_voto==true){
+
+            this.voto= true;
+        }
+    }
 
 }
